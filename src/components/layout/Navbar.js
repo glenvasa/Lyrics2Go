0a4256d52b8f3ef097f0e6632be02fa0c0d897a0
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-01-23 14:51:29 -0500

    Added NavMusic component in Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import NavMusic from "./NavMusic";
+import LTG from "./LTG-1.mp3";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark mb-5">
-      <span className="navbar-brand mb-0 h1 mx-auto">LyricFinder</span>
+      <div className="navbar-brand text-warning mb-0 text-center mx-auto">
+        <NavMusic url={LTG} />
+        <span className="h3">GetLyrics2Go</span>
+        <br />
+        <span className="h5 text-white">Lyrics ... on the Fly</span>
+      </div>
     </nav>
   );
 };
